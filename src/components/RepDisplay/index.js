6aee9b92fd0fe3233c886c846c56ceae91261b8c
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-08-21 20:42:43 -0600

    removed excess styled component that could be condensed

--- src/components/RepDisplay/index.js
+++ src/components/RepDisplay/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import styled from "styled-components"
 import Card from './Card';
 
-const DisplayDiv = styled.div`
-    display: flex;
-    flex-flow: row wrap;
-`
 
 const RepDisplay = (props) =>{
     const { results } = props;
 
     return(
-        <section className='RepDisplay'>
-            <DisplayDiv>
-                {results.map((result, idx) => { 
-                    return <Card result={result} key={idx} />
-                })}
-            </DisplayDiv>
+        <section className='RepDisplay' style={{display: "flex", flexFlow: "row wrap"}} >
+            {results.map((result, idx) => { 
+                return <Card result={result} key={idx} />
+            })}
         </section>
     )
 } 
