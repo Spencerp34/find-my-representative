40b891ceb5cde36534ca220b337222a9616f04d3
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-08-21 18:12:43 -0600

    set up libraries and got started

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material"
+import states from './components/states';
 
 function App() {
+  const [category, setCategory] = useState("Senator");
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FormControl fullWidth sx={{width: "500px", margin: "25px"}}>
+          <InputLabel>Category</InputLabel>
+        <Select label="Category" value={category} onChange={handleChange} >
+          <MenuItem value={"senators"} >Senators</MenuItem>
+          <MenuItem value={"representatives"} >Representatives</MenuItem>
+        </Select>
+        </FormControl>
+        {states.map((state, idx) =>{
+          return <h5>{state.name}</h5>
+        } )}
       </header>
     </div>
   );
